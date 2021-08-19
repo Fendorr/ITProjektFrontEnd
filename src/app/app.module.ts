e4bc822e089ff6cc5e377fcc3e2ba65f547e6c64
JOKeRiino <simontroester@yahoo.com>
2021-08-19 15:35:05 +0200

    material imports in material.module.ts exportiert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,12 @@ import { FormsModule } from '@angular/forms';
 //* Component Imports
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
-
-//* Angular Material Imports
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 import { ProjectComponent } from './project/project.component';
 import { NewProjectComponent } from './project/new-project/new-project.component';
 import { TestComponent } from './test/test.component';
 import { StartprojectComponent } from './project/startproject/startproject.component';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [ //? Wenn man hier einen Componentnamen reinschreibt, passiert der Import meist von selbst.
@@ -38,9 +34,7 @@ import { ProjectsListComponent } from './projects-list/projects-list.component';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
+    MaterialModule
   ],
   providers: [
     {
